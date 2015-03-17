81ba49731e30e08ced400e623262ede898917b62
dusgnlfl <dusgnlfl@naver.com>
2015-03-17 18:47:31 +0900

    구글맵지도와 웹페이지를 보일수있는 기능구현

--- Mytour/app/src/main/java/com/example/yeonsang/mytour/SubActivity.java
+++ Mytour/app/src/main/java/com/example/yeonsang/mytour/SubActivity.java
@@ -1,7 +1,13 @@
 package com.example.yeonsang.mytour;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.net.URI;
 
 public class SubActivity extends Activity{
 
@@ -9,5 +15,23 @@ public class SubActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sublayout);
     }
-
+    public void mOnClick(View v) {
+        switch (v.getId()) {
+            case R.id.map1:
+                Uri uri= Uri.parse("geo:37.551472,126.988426");
+                Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+                break;
+            case R.id.info1:
+                String url = "http://www.nseoultower.com";
+                Intent intent2 = new Intent(Intent.ACTION_VIEW);
+                intent2.setData(Uri.parse(url));
+                startActivity(intent2);
+                break;
+            case R.id.mainpage:
+                Toast.makeText(getApplicationContext(), "남산을 보고 오셨습니다..", Toast.LENGTH_LONG).show();
+                finish();
+                break;
+        }
+    }
 }
